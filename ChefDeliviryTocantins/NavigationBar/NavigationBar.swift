d1bcb9ef98650386b8fef520c62e43126c17f20e
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-03-15 21:44:17 -0300

    Commit - Implementando a NavigationBar no app

---
+++ ChefDeliviryTocantins/NavigationBar/NavigationBar.swift
@@ -0,0 +1,18 @@
+//
+//  NavigationBar.swift
+//  ChefDeliviryTocantins
+//
+//  Created by Sósthenes Oliveira Lima on 15/03/24.
+//
+
+import SwiftUI
+
+struct NavigationBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationBar()
+}
