21ad29c1fe0b3b189e1d7d3901e0431a10cc4775
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-03-15 21:25:39 -0300

    Initial Commit

---
+++ ChefDeliviryTocantins/ChefDeliviryTocantinsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefDeliviryTocantinsApp.swift
+//  ChefDeliviryTocantins
+//
+//  Created by Sósthenes Oliveira Lima on 15/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ChefDeliviryTocantinsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChefDeliviryTocantins/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChefDeliviryTocantins
+//
+//  Created by Sósthenes Oliveira Lima on 15/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+            Text("Teste")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+        .previewLayout(.sizeThatFits)
+}
