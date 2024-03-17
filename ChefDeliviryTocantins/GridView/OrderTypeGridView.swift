066ce5591bfbbef65d6505b42e28546e5f879db3
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-03-17 14:56:29 -0300

    Commit - Entendendo o Grid01

---
+++ ChefDeliviryTocantins/GridView/OrderTypeGridView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderTypeGridView.swift
+//  ChefDeliviryTocantins
+//
+//  Created by Sósthenes Oliveira Lima on 17/03/24.
+//
+
+import SwiftUI
+
+struct OrderTypeGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderTypeGridView()
+}
