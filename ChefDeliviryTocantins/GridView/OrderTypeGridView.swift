9690534e2ca6e61efbf314d469a61119e9265dcb
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-03-20 20:52:30 -0300

    Commit - Utilizando GridItem02

--- ChefDeliviryTocantins/GridView/OrderTypeGridView.swift
+++ ChefDeliviryTocantins/GridView/OrderTypeGridView.swift
@@ -15,14 +15,18 @@ struct OrderTypeGridView: View {
  
     
     var body: some View {
-        LazyHGrid(rows: gridLayout ) {
+        LazyHGrid(rows: gridLayout, spacing: 15 ) {
             ForEach(ordersMock) { orderItem in
                 OrderTypeView(orderType: orderItem)
             }
         }
+        .frame(height: 200)
+        .padding(.horizontal, 15)
+        .padding(.top, 15)
     }
 }
 
 #Preview {
     OrderTypeGridView()
+        .previewLayout(.sizeThatFits)
 }
