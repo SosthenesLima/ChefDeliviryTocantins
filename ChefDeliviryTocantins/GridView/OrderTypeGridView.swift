f8afcdc16ea6bff81ed087d52585bf16f99d5a0e
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-03-19 21:25:47 -0300

    Commit - View de tipos de restaurantes02

--- ChefDeliviryTocantins/GridView/OrderTypeGridView.swift
+++ ChefDeliviryTocantins/GridView/OrderTypeGridView.swift
@@ -9,15 +9,15 @@ import SwiftUI
 
 struct OrderTypeGridView: View {
     
-    let ordens = ["Restaurantes", "Farmácia", "Descontos", "Gourmet", "Mercados", "Pet", "Bebidas"]
+ 
     
     var body: some View {
         LazyHGrid(rows: [
             GridItem(.fixed(100)),
             GridItem(.fixed(100))
         ]) {
-            ForEach(ordens, id: \.self) { orderItem in
-             Text(orderItem)
+            ForEach(ordersMock) { orderItem in
+                Text(orderItem.name)
             }
         }
     }
