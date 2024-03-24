1944218630875bfaffb0667bb2f5ae6a7673109f
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-03-24 19:18:55 -0300

    Commit - Inserindo a primeira imagem do carrossel

---
+++ ChefDeliviryTocantins/CarouselView/CarouselItemView.swift
@@ -0,0 +1,18 @@
+//
+//  CarouselItemView.swift
+//  ChefDeliviryTocantins
+//
+//  Created by Sósthenes Oliveira Lima on 24/03/24.
+//
+
+import SwiftUI
+
+struct CarouselItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarouselItemView()
+}
