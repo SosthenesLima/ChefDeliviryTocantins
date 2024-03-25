68aa4ef4e50807f5987a2a14a94daba3e8e5a448
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-03-25 16:48:30 -0300

    Commit - Trabalhando com TabView para implementar o carrossel

---
+++ ChefDeliviryTocantins/CarouselView/CarouselTabView.swift
@@ -0,0 +1,18 @@
+//
+//  CarouselTabView.swift
+//  ChefDeliviryTocantins
+//
+//  Created by Sósthenes Oliveira Lima on 25/03/24.
+//
+
+import SwiftUI
+
+struct CarouselTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarouselTabView()
+}
