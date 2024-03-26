dc2a8a954327924505c7d38eecdcf86bd9489db5
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-03-26 17:44:39 -0300

    Commit - Criando a View de restaurantes01

---
+++ ChefDeliviryTocantins/StoresView/StoreItemView.swift
@@ -0,0 +1,18 @@
+//
+//  StoreItemView.swift
+//  ChefDeliviryTocantins
+//
+//  Created by Sósthenes Oliveira Lima on 26/03/24.
+//
+
+import SwiftUI
+
+struct StoreItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoreItemView()
+}
